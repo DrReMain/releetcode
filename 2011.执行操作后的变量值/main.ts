e101aad0be84fa7fb9e9438ef04f77d6c2d124cd
DrReMain <51071534@qq.com>
2023-12-15 10:21:29 +0800

    update

--- 2011.执行操作后的变量值/main.ts
+++
@@ -1,9 +0,0 @@
-function finalValueAfterOperations(operations: string[]): number {
-  let result = 0;
-
-  operations.forEach((ope) => {
-    result += ",".charCodeAt(0) - ope.charCodeAt(1);
-  });
-
-  return result;
-}
