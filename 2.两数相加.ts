fadc62b446abe92522f7fa131e074979fab902a4
DrReMain <51071534@qq.com>
2023-12-15 14:37:37 +0800

    update

---
+++ 2.两数相加.ts
@@ -0,0 +1,43 @@
+/*
+ * @lc app=leetcode.cn id=2 lang=typescript
+ *
+ * [2] 两数相加
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function addTwoNumbers(l1: ListNode | null, l2: ListNode | null): ListNode | null {
+    let dummyHead = new ListNode(0);
+
+    let p = l1, q = l2, curr = dummyHead;
+    let carry = 0;
+    while (p || q) {
+        let x = p ? p.val : 0;
+        let y = q ? q.val : 0;
+        let sum = carry + x + y;
+        carry = Math.floor(sum / 10);
+        curr.next = new ListNode(sum % 10);
+
+        curr = curr.next;
+        p = p?.next;
+        q = q?.next;
+    }
+    if (carry > 0) {
+        curr.next = new ListNode(carry);
+    }
+
+    return dummyHead.next;
+};
+// @lc code=end
+
