2b837107a042c9b29cadf2653a2f7c006131914a
DrReMain <51071534@qq.com>
2023-12-28 20:43:50 +0800

    update

---
+++ 4.寻找两个正序数组的中位数.rs
@@ -0,0 +1,43 @@
+/*
+ * @lc app=leetcode.cn id=4 lang=rust
+ *
+ * [4] 寻找两个正序数组的中位数
+ */
+
+// @lc code=start
+impl Solution {
+    pub fn find_median_sorted_arrays(nums1: Vec<i32>, nums2: Vec<i32>) -> f64 {
+        let n = nums1.len() + nums2.len();
+        let (mut i, mut j) = (0, 0);
+        let (mut r1, mut r2) = (0, 0);
+        while i + j < n / 2 + 1 {
+            let mid = match (nums1.get(i), nums2.get(j)) {
+                (Some(&x), Some(&y)) => {
+                    if x > y {
+                        j += 1;
+                        y
+                    } else {
+                        i += 1;
+                        x
+                    }
+                }
+                (Some(&x), None) => {
+                    i += 1;
+                    x
+                }
+                (None, Some(&y)) => {
+                    j += 1;
+                    y
+                }
+                (None, None) => unreachable!(),
+            };
+            r1 = r2;
+            r2 = mid;
+        }
+        if n % 2 == 0 {
+            return (r1 as f64 + r2 as f64) / 2.0;
+        }
+        r2 as f64
+    }
+}
+// @lc code=end
