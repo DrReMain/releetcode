363329c29340235433e56d2fe08545086c7aaf9f
ReMain(){const CYBER;} <51071534@qq.com>
2024-01-12 17:45:06 +0000

    update

---
+++ playground-golang/examples/n206/n206.go
@@ -0,0 +1,19 @@
+package n206
+
+import (
+	"drremain.cn/playground-golang/DataStructure"
+)
+
+type ListNode = DataStructure.ListNode[int]
+
+func reverseList(head *ListNode) *ListNode {
+	var prev *ListNode
+	node := head
+	for node != nil {
+		tmp := node.Next
+		node.Next = prev
+		prev = node
+		node = tmp
+	}
+	return prev
+}
