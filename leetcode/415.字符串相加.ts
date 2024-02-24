deb671764b55fcb69bcc6b52f10e25bf786bd8f8
DrReMain <51071534@qq.com>
2024-02-24 16:09:19 +0800

    update

--- leetcode/415.字符串相加.ts
+++ leetcode/415.字符串相加.ts
@@ -6,21 +6,19 @@
 
 // @lc code=start
 function addStrings(num1: string, num2: string): string {
-    let [i, j, carry] = [num1.length - 1, num2.length - 1, 0];
+    let [i1, i2, carry] = [num1.length-1, num2.length-1, 0];
     let ret = "";
-    
-    while (i >= 0 || j >= 0 || carry > 0) {
+    while (i1 >= 0 || i2 >= 0 || carry > 0) {
         let [n1, n2] = [0, 0];
-        if (i >= 0) n1 = num1[i].charCodeAt(0) - '0'.charCodeAt(0);
-        if (j >= 0) n2 = num2[j].charCodeAt(0) - '0'.charCodeAt(0);
+        if (i1 >= 0) n1 = num1.charCodeAt(i1) - 48;
+        if (i2 >= 0) n2 = num2.charCodeAt(i2) - 48;
 
         const sum = n1 + n2 + carry;
         ret = sum%10 + ret;
-        carry = Math.floor(sum/10);
-        i--;
-        j--;
+        carry = Math.trunc(sum/10);
+        --i1;
+        --i2;
     }
-
     return ret;
 };
 // @lc code=end
