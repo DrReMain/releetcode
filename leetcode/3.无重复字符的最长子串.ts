263bd9d3451ef193fc4e7435d6b2b76f281e3273
DrReMain <51071534@qq.com>
2024-02-24 15:02:43 +0800

    update

--- leetcode/3.无重复字符的最长子串.ts
+++ leetcode/3.无重复字符的最长子串.ts
@@ -6,17 +6,18 @@
 
 // @lc code=start
 function lengthOfLongestSubstring(s: string): number {
-    const m = new Map<string, boolean>();
+    let l = s.length;
+    if (l < 2) return l;
+
+    const m = new Map<string, number>();
     let ret = 0;
-    let cur = 0;
-    for (let i = 0; i < s.length; ++i) {
-        if (i > 0) m.set(s[i - 1], false);
-        while (cur < s.length && !m.get(s[cur])) {
-            m.set(s[cur], true);
-            cur++;
-        }
-        ret = Math.max(ret, cur - i);
+    for (let cur = 0, i = 0; i < l; ++i) {
+        if (m.has(s[i]))
+            cur = Math.max(cur, m.get(s[i])! + 1);
+        ret = Math.max(ret, i - cur + 1);
+        m.set(s[i], i);
     }
+
     return ret;
 };
 // @lc code=end
