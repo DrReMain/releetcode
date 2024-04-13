d641fd1bc62b51c540760e4abba626bce2705142
DrReMain <51071534@qq.com>
2024-04-13 16:09:07 +0800

    update

--- leetcode/3.无重复字符的最长子串.rs
+++ leetcode/3.无重复字符的最长子串.rs
@@ -8,19 +8,15 @@
 impl Solution {
     pub fn length_of_longest_substring(s: String) -> i32 {
         use std::collections::HashMap;
-        let s = s.as_bytes();
-        let mut m: HashMap<u8, bool> = HashMap::new();
-        let mut ret = 0;
-        let mut cur = 0;
+        let s = s.chars().collect::<Vec<char>>();
+        let mut m: HashMap<char, usize> = HashMap::new();
+        let (mut ret, mut cur) = (0, 0);
         for (i, c) in s.iter().enumerate() {
-            if i > 0 {
-                m.insert(s[i - 1], false);
+            if let Some(idx) = m.get(c) {
+                cur = cur.max(idx+1);
             }
-            while cur < s.len() && !m.get(&s[cur]).map_or(false, |v| *v) {
-                m.insert(s[cur], true);
-                cur += 1;
-            }
-            ret = ret.max(cur - i);
+            ret = ret.max(i+1-cur);
+            m.insert(*c, i);
         }
         ret as i32
     }
